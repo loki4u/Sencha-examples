27c989d37b671a7ccf8de82bd9d20bb8f9bcf068
lokesh <lokesh@webmobi.com>
2014-12-19 11:21:11 +0530

    added push notification in sencha touch

---
+++ PushApp/app/view/Main.js
@@ -0,0 +1,11 @@
+Ext.define('PushApp.view.Main', {
+    extend: 'Ext.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        html:'panel html'
+    }
+});
